dd0970c30b521ac2be39b692d69068769767aee3
Chad Lillquist <conrad0119@yahoo.com>
2013-01-24 10:41:40 -0700

    rake commands

---
+++ rakefile.rb
@@ -0,0 +1,22 @@
+desc 'Clean Site'
+task :clean do
+  sh 'rm -rf _site/'
+  puts 'Site Folder Cleaned'
+end
+
+desc 'Update Repo'
+task :update do
+  sh 'git pull origin master'
+  puts 'pulled from master'
+end
+
+desc 'Build Site'
+task :build do
+  sh 'jekyll'
+  puts 'Site Built'
+end
+
+desc 'Test Cron'
+task :build do
+  puts 'test cron'
+end
