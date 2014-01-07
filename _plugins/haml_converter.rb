62d1e9a4264eb9a248fac1f495ae60d4b52518d1
Chad Lillquist <conrad0119@yahoo.com>
2014-01-06 18:38:51 -0700

    unpublished

--- _plugins/haml_converter.rb
+++ _plugins/haml_converter.rb
@@ -18,22 +18,4 @@ module Jekyll
     end
   end
 
-  require 'sass'
-  class ScssConverter < Converter
-    safe true
-    priority :low
-
-     def matches(ext)
-      ext =~ /scss/i
-    end
-
-    def output_ext(ext)
-      ".css"
-    end
-
-    def convert(content)
-      engine = Sass::Engine.new(content, :syntax => :scss, :load_paths => ["./css/"])
-      engine.render
-    end
-  end
 end
